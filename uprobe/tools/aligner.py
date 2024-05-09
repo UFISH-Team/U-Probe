8ed4771eed2908d6c4acf47b5bf01722843049a1
Zjhlsp199409 <876500972@qq.com>
2024-05-09 10:32:43 +0800

    add index tools

---
+++ uprobe/tools/aligner.py
@@ -0,0 +1,9 @@
+import fire
+
+def build_blast_index(fa_path, output_path):
+    print("hello")
+
+if __name__ == "__main__":
+    fire.Fire({
+        "build_blast_index": build_blast_index
+    })
