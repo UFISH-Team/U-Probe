2293ea5a9d84594a46d3a1b622849ce1687ffb76
jshn2022 <jshn2022@163.com>
2025-04-12 14:47:25 +0800

    Remove obsolete CSV files and update genome paths in YAML configuration for consistency; enhance workflow parameters to accept multiple input types.

--- uprobe/__main__.py
+++ uprobe/__main__.py
@@ -3,18 +3,21 @@ from pathlib import Path
 from .workflow import construct_workflow
 import sys
 import os
+import typing as T
 
 project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(project_root)
 
 def main():
     parser = argparse.ArgumentParser(description="Run the workflow for double hybrid RCA.")
-    parser.add_argument("--genomes_yaml", type=Path, required=True,
+    parser.add_argument("--genomes_yaml", type=T.Union[Path, str, dict], required=True,
                         help="Path to the genomes YAML file.")
-    parser.add_argument("--protocol_yaml", type=Path, required=True,
+    parser.add_argument("--protocol_yaml", type=T.Union[Path, str, dict], required=True,
                         help="Path to the protocol YAML file.")
     parser.add_argument("--output_csv", type=Path, required=True,
                         help="Path to the output CSV file.")
+    parser.add_argument("--raw_results_csv", type=Path, default=None,
+                        help="Path to the raw results CSV file.")
     parser.add_argument("--workdir", type=Path, default=Path("."),
                         help="Working directory (default: current directory).")
 
@@ -24,10 +27,13 @@ def main():
         args.protocol_yaml,
         args.genomes_yaml,
         args.output_csv,
-        args.workdir
+        args.workdir,
+        args.raw_results_csv
     )
 
-    workflow.run()
+    workflow()
+
+
 
 if __name__ == "__main__":
     main()
