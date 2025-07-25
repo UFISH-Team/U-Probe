730e0c7603b81e8ae6c6622481b40dc974b31b4d
qzhang <105474865+jshn2022@users.noreply.github.com>
2025-07-25 17:57:11 +0800

    Update __init__.py

--- uprobe/__init__.py
+++ uprobe/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.1.0'
+from .api import run_workflow, build_genome_index, get_gene_barcodes
+
+__all__ = [
+    "run_workflow",
+    "build_genome_index",
+    "get_gene_barcodes"
+]
