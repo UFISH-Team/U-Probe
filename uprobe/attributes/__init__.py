862f924f977e32943ee294c166c576b377ae605a
qzhang <105474865+jshn2022@users.noreply.github.com>
2025-07-25 16:03:05 +0800

    Update __init__.py

--- uprobe/attributes/__init__.py
+++ uprobe/attributes/__init__.py
@@ -12,7 +12,6 @@ def add_attributes(
         df_probes: pd.DataFrame,
         protocol: dict,
         genome: dict,
-        workdir: Path
 ) -> pd.DataFrame:
     attributes: dict = protocol['attributes']
     task_id = secrets.token_hex(6)  
@@ -43,13 +42,10 @@ def add_attributes(
                 index_prefix = fasta_path.parent / 'bowtie2_genome' / fasta_path.stem
                 tmp_dir = Path("tmp")
                 tmp_dir.mkdir(exist_ok=True, parents=True)
-                vals = df_probes.apply(
-                    lambda row: count_n_bowtie2_aligned_genes(
-                        str(tmp_dir), {f"{row['exon_name']}_{row['start']}": row[actual_target]}, task_id,
-                        str(index_prefix),
-                        attr.get("threads", 10), 
-                    ), axis=1
-                )
+                recname2seq = {f"{row['exon_name']}_{row['start']}": row['target_region'] for (_, row) in df_probes.iterrows()}
+                n_mapped_genes = count_n_bowtie2_aligned_genes(tmp_dir, recname2seq, task_id, index_prefix, attr.get("min_mapq", 30), attr.get("threads", 10))
+                n_mapped_genes = [n_mapped_genes[f"{row['exon_name']}_{row['start']}"] for _, row in df_probes.iterrows()]
+                df_probes[attr_name] = n_mapped_genes
                 import shutil
                 shutil.rmtree(tmp_dir)
             else:
@@ -58,20 +54,24 @@ def add_attributes(
                 )
         elif attr_type == "annealing_temperature":
             vals = df_probes[actual_target].apply(cal_temp).round(2)
+            df_probes[attr_name] = vals
         elif attr_type == "gc_content":
             vals = df_probes[actual_target].apply(cal_gc_content).round(2)
+            df_probes[attr_name] = vals
         elif attr_type == "fold_score":
             vals = df_probes[actual_target].apply(cal_target_fold_score).round(2)
+            df_probes[attr_name] = vals
         elif attr_type == "self_match":
             vals = df_probes[actual_target].apply(cal_self_match).round(2)
+            df_probes[attr_name] = vals
         elif attr_type == "blocks":
             if 'start' not in df_probes.columns:
                 print(f"Warning: 'start' column not found for attribute '{attr_name}'")
                 continue
             vals = df_probes.apply(lambda row: cal_target_blocks(row[actual_target], row['start']), axis=1)
+            df_probes[attr_name] = vals
         else:
             raise NotImplementedError(
                 f"Attribute type {attr_type} is not implemented."
             )
-        df_probes[attr_name] = vals
     return df_probes
