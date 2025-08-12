2aceaab5721c7ffb5e7ac5ddc16257d8cd376cbc
qzhang <105474865+jshn2022@users.noreply.github.com>
2025-08-12 16:58:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ classifiers = [
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
-    "Programming Language :: Python :: 3.11",
     "License :: OSI Approved :: MIT License",
     "Intended Audience :: Science/Research",
 ]
@@ -105,3 +105,4 @@ setup(
         ],
     },
 )
+
