70ff838f3fd59f6040e6692f14d2762c6c672d97
qzhang <105474865+jshn2022@users.noreply.github.com>
2025-09-11 21:54:10 +0800

    Update __init__.py

--- uprobe/report/__init__.py
+++ uprobe/report/__init__.py
@@ -2,8 +2,8 @@
 Report generation module for U-Probe.
 """
 
-from .plot import generate_plot_report, generate_summary_plots
+from .plot import generate_plot_report
 from .html import save_html_report
 
-__all__ = ['generate_plot_report', 'generate_summary_plots', 'save_html_report']
+__all__ = ['generate_plot_report', 'save_html_report']
 
