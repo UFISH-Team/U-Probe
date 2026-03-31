4f585c7116b714b0aeff7e336f48430a7270dc9e
qzhang <105474865+jshn2022@users.noreply.github.com>
2026-03-31 23:40:01 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,109 +1,68 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+
+sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
-project = 'U-Probe'
-copyright = '2025, Qian Zhang'
-author = 'Qian Zhang'
+project = "U-Probe"
+author = "Qian Zhang"
+copyright = "2025-2026, Qian Zhang"
 
-# The version info for the project you're documenting
-# Import version from the package
 try:
     from uprobe import __version__
     version = __version__
     release = __version__
 except ImportError:
-    version = '1.0.0'
-    release = '1.0.0'
+    version = "1.0.0"
+    release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode', 
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.ifconfig',
-    'myst_parser',
-    'sphinx_copybutton',
-    'sphinx_click',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx_click",
+    "sphinx_design",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-# The suffix(es) of source filenames.
 source_suffix = {
-    '.rst': None,
-    '.md': 'myst_parser',
+    ".rst": None,
+    ".md": "myst_parser",
 }
 
-# The master toctree document.
-master_doc = 'index'
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'default'
+master_doc = "index"
+pygments_style = "default"
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-html_theme_options = {
-    'canonical_url': '',
-    'analytics_id': '',
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'logo_only': False,
-    # Toc options
-    'collapse_navigation': False,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+html_theme = "furo"
+html_title = project
+html_short_title = project
 
 # Add any paths that contain custom static files (such as style sheets) here
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names. (Commented out to use theme defaults)
-# html_sidebars = {
-#     '**': [
-#         'about.html',
-#         'navigation.html',
-#         'relations.html',
-#         'searchbox.html',
-#         'donate.html',
-#     ]
-# }
+# Logo and Favicon
+html_favicon = "_static/logo.svg"
 
-# The name for this set of Sphinx documents.
-html_title = f'{project} v{version} Documentation'
+# Furo Theme Options
+html_theme_options = {
+    "source_repository": "https://github.com/UFISH-Team/U-Probe",
+    "source_branch": "main",
+    "source_directory": "docs/source/",
+    "sidebar_hide_name": True,
+    "top_of_page_button": "edit",
+    "light_logo": "logo.svg",
+    "dark_logo": "logo.svg",
+}
 
-# A shorter title for the navigation bar.
-html_short_title = 'U-Probe Docs'
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-html_logo = None
-
-# The name of an image file (relative to this directory) to use as a favicon of
-# the docs.
-html_favicon = None
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'U-Probedoc'
+htmlhelp_basename = "UProbedoc"
 
 # -- Extension configuration -------------------------------------------------
 
@@ -113,69 +72,42 @@ napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = False
 napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
 napoleon_use_param = True
 napoleon_use_rtype = True
-napoleon_type_aliases = None
 
 # Autodoc settings
 autodoc_default_options = {
-    'members': True,
-    'member-order': 'bysource',
-    'special-members': '__init__',
-    'undoc-members': True,
-    'exclude-members': '__weakref__'
+    "members": True,
+    "member-order": "bysource",
+    "special-members": "__init__",
+    "undoc-members": True,
+    "exclude-members": "__weakref__",
 }
+autodoc_typehints = "description"
 
 # Intersphinx mapping
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'pandas': ('https://pandas.pydata.org/docs/', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
-    'click': ('https://click.palletsprojects.com/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "click": ("https://click.palletsprojects.com/en/stable/", None),
 }
+
+intersphinx_timeout = 5
 
 # MyST parser settings
 myst_enable_extensions = [
-    'colon_fence',
-    'deflist',
-    'dollarmath',
-    'html_image',
-    'linkify',
-    'replacements',
-    'smartquotes',
-    'substitution',
-    'tasklist',
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "substitution",
+    "tasklist",
 ]
 
 # Copy button settings
-copybutton_prompt_text = r'>>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: '
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
 copybutton_prompt_is_regexp = True
-
-# Todo extension settings
-todo_include_todos = True
-
-# -- Options for LaTeX output ------------------------------------------------
-latex_elements = {}
-
-# Grouping the document tree into LaTeX files. List of tuples
-latex_documents = [
-    (master_doc, 'U-Probe.tex', 'U-Probe Documentation',
-     'UFISH Team', 'manual'),
-]
-
-# -- Options for manual page output ------------------------------------------
-man_pages = [
-    (master_doc, 'uprobe', 'U-Probe Documentation',
-     [author], 1)
-]
-
-# -- Options for Texinfo output ----------------------------------------------
-texinfo_documents = [
-    (master_doc, 'U-Probe', 'U-Probe Documentation',
-     author, 'U-Probe', 'Universal Probe Design Tool.',
-     'Miscellaneous'),
-] 
